f8aadc7c05ee1dd062c5edf349bcb039782b40f1
Yulong Wu <yulong@radixdlt.com>
2022-04-06 09:34:15 +0100

    Add example API schema and codegen

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello World");
+import { paths } from "./pte";
+import { Fetcher } from "openapi-typescript-fetch";
+
+// declare fetcher for paths
+const fetcher = Fetcher.for<paths>();
+
+// global configuration
+fetcher.configure({
+    baseUrl: "http://api.example.com/v1",
+    init: {
+        headers: {
+        },
+    },
+    use: []
+});
+
+// create fetch operations
+const getUsers = fetcher.path("/users").method("get").create();
+
+// fetch
+const users = getUsers({});
+console.log(users);
