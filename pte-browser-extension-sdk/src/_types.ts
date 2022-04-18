b9263a454b1f2d4e98585741112901d68c2412c3
Yulong Wu <yulong@radixdlt.com>
2022-04-18 22:26:32 +0100

    Remove unneeded message types for PTE

--- pte-browser-extension-sdk/src/_types.ts
+++ pte-browser-extension-sdk/src/_types.ts
@@ -1,95 +1,40 @@
-export enum SignerMessageType {
-  SIGN_TX,
-}
-
-export type SignerMessage<T extends SignerMessageType> = {
-  type: T;
-  tx: string;
-};
-
-export type Signer = {
-  sign: <T extends SignerMessageType>(tx: SignerMessage<T>) => Promise<string>;
-};
-
+/**
+ * Represents the type of an action.
+ */
 export enum ActionType {
-  Connect = "connect",
-  ConnectSuccess = "connectSuccess",
-  ConnectFail = "connectFail",
-  HandShake = "handShake",
-  HandShakeSuccess = "handShakeSuccess",
-  Sign = "sign",
-  SignSuccess = "signSuccess",
-  GetAlias = "getAlias",
-  GetAliasSuccess = "getAliasSuccess",
-  GetAliasFail = "getAliasFail",
+  GetAccountAddress = "getAccountAddress",
+  GetAccountAddressSuccess = "getAccountAddressSuccess",
+  GetAccountAddressFailure = "getAccountAddressFailure",
+
+  SignTransaction = "signTransaction",
+  SignTransactionSuccess = "signTransactionSuccess",
+  SignTransactionFailure = "signTransactionFailure",
 }
 
+/**
+ * Represents an action.
+ */
 export type Action<T extends ActionType, P> = {
   type: T;
   payload: P;
   id: string;
 };
 
-export type Connect = Action<
-  ActionType.Connect,
-  {
-    permissions: string[];
-  }
->;
+export type GetAccountAddress = Action<ActionType.GetAccountAddress, string>;
+export type GetAccountAddressSuccess = Action<ActionType.GetAccountAddressSuccess, string>;
+export type GetAccountAddressFailure = Action<ActionType.GetAccountAddressFailure, string>;
 
-export type ConnectSuccess = Action<ActionType.ConnectSuccess, void>;
-
-export enum ConnectFailReason {
-  RejectedByUser = "rejectedByUser",
-  Timeout = "timeout",
-}
-
-export type ConnectFail = Action<
-  ActionType.ConnectFail,
-  {
-    reason: ConnectFailReason;
-  }
->;
-
-export type HandShake = Action<ActionType.HandShake, void>;
-
-export type HandShakeSuccess = Action<
-  ActionType.HandShakeSuccess,
-  { connected: boolean }
->;
-
-export type SignTx = Action<
-  ActionType.Sign,
-  SignerMessage<SignerMessageType.SIGN_TX>
->;
-
-export type SignTxSuccess = Action<ActionType.Sign, string>;
-
-export enum GetAliasFailReason {
-  Permission = "Permission",
-  Connection = "Connection",
-}
-export type GetAlias = Action<ActionType.GetAlias, void>;
-export type GetAliasSuccess = Action<
-  ActionType.GetAliasSuccess,
-  { id: string; name: string }
->;
-export type GetAliasFail = Action<
-  ActionType.GetAliasFail,
-  { reason: GetAliasFailReason }
->;
+export type SignTransaction = Action<ActionType.SignTransaction, string>;
+export type SignTransactionSuccess = Action<ActionType.SignTransactionSuccess, string>;
+export type SignTransactionFailure = Action<ActionType.SignTransactionFailure, string>;
 
 export type ActionTypes =
-  | Connect
-  | ConnectSuccess
-  | ConnectFail
-  | SignTx
-  | SignTxSuccess
-  | HandShake
-  | HandShakeSuccess
-  | GetAlias
-  | GetAliasSuccess
-  | GetAliasFail;
+  GetAccountAddress
+  | GetAccountAddressSuccess
+  | GetAccountAddressFailure
+  | SignTransaction
+  | SignTransactionSuccess
+  | SignTransactionFailure
 
 export enum MessageTarget {
   Extension,
