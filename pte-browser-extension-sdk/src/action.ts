73b608024bdb4956e4d83f30326d3f7f04f27d0b
Yulong Wu <yulong@radixdlt.com>
2022-04-19 12:10:42 +0100

    Update SDK

--- pte-browser-extension-sdk/src/action.ts
+++ pte-browser-extension-sdk/src/action.ts
@@ -1,4 +1,4 @@
-import { ActionType, ActionTypes } from "./_types";
+import { ActionType, ActionTypes, MessageStoreItem } from "./_types";
 
 export const sendAction = ({ type, payload }: Omit<ActionTypes, "id">) => {
   const event = new CustomEvent("radix#chromeExtension#send", {
@@ -16,7 +16,8 @@ export const waitForAction = async <SuccessType extends ActionTypes>(
     window.addEventListener(
       "radix#chromeExtension#receive",
       (event) => {
-        const action = (event as CustomEvent<ActionTypes>).detail;
+        const { action } = (event as CustomEvent<MessageStoreItem<ActionTypes>>)
+          .detail;
 
         if (action.type === successType) resolve(action as SuccessType);
         else if (errorTypes?.includes(action.type)) reject(action);
